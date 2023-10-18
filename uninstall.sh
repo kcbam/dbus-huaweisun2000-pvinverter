18db5ff4d5d182765060aac30b0a40fcc6514e48
Christian Kinsch <46842731+kcbam@users.noreply.github.com>
2023-10-18 16:22:06 +0200

    Update uninstall.sh
    changed directory

--- uninstall.sh
+++ uninstall.sh
@@ -3,9 +3,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 filename=/data/rc.local
 rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise dbus-sun2000-pvinverter')
+kill $(pgrep -f 'supervise dbus-huaweisun2000-pvinverter')
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
 STARTUP=$SCRIPT_DIR/install.sh
-#sed -i "\~/data/etc/dbus-sun2000-pvinverter/install.sh~d" $filename
 sed -i "\~$STARTUP~d" $filename
