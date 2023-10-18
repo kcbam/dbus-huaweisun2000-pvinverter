f54a47469dc680371d1122dc015f2583ae944045
Christian Kinsch <46842731+kcbam@users.noreply.github.com>
2023-10-18 16:21:02 +0200

    Update restart.sh
    changed directory

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "SCRIPT_DIR: $SCRIPT_DIR"
 rm $SCRIPT_DIR/current.log
 
-kill $(pgrep -f "python $SCRIPT_DIR/dbus-sun2000-pvinverter.py")
+kill $(pgrep -f "python $SCRIPT_DIR/dbus-huaweisun2000-pvinverter.py")
